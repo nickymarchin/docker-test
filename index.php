91f5027ea382ec4dbedebfd47910eee879b8f75d
Nikolay Marchin <n.marchin@ITRServices.eu>
2020-02-04 14:26:47 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+	<title>Docker?</title>
+	
+	<script>
+		function myFunction1() {
+			var x = document.getElementById("myDIV1");
+			if (x.style.display === "none") {
+				x.style.display = "block";
+			} else {
+				x.style.display = "none";
+			}
+		}
+		
+		function myFunction2() {
+			var x = document.getElementById("myDIV2");
+			if (x.style.display === "none") {
+				x.style.display = "block";
+			} else {
+				x.style.display = "none";
+			}
+		}
+	</script>
+</head>
+
+<body>
+	<button onclick="myFunction1()">Hello</button>
+	<div id="myDIV1" style="display:none;">
+	  Hi
+	</div>
+	
+	<button onclick="myFunction2()">Goodbye</button>
+	<div id="myDIV2" style="display:none;">
+	  Bye
+	</div>
+</body>
+</html>
+
+<?php
+
+$host = 'db'; //this is the service name
+$user = 'devuser';
+$password = 'devpass';
+
+$db = 'test_db';
+
+$connection = new mysqli($host, $user, $password, $db);
+
+if($connection->connect_error){
+	echo 'Connection failed!' . $connection->connect_error;
+} else {
+	
+	$sql = "SELECT name FROM users";
+	
+	if($result = mysqli_query($connection, $sql)){
+		
+		echo "<ol>";
+		while($row = mysqli_fetch_row($result)){
+				echo "<li>" . $row[0] . "</li>";
+		}
+		echo "</ol>";
+		
+		mysqli_free_result($result);
+	}
+	
+}
+
+mysqli_close($connection);
+
+?>
